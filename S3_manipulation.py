8199a93cde6f7e48763e885526687fce6448cab6
Dorus <30291690+dwrolvink@users.noreply.github.com>
2017-07-20 09:29:56 +0200

    v 1.2
    first good working version after v1. Includes build-up and destroy option at launch

--- S3_manipulation.py
+++ S3_manipulation.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import boto3
 
 #
@@ -9,13 +9,23 @@ import boto3
 #
 #
 
+
 # Setup settings
 bucket_name = 'bb1mb'
 file_name = 'par.png'
 file_folder_location = 'C:\\Users\\dwrol\\Desktop\\temp\\'
-# Delete bucket after run
-delBucket = False
+# Setup bucket with file, or delete bucket ("create', "destroy")
+RunMode = "notSet"
 
+
+# Load CMD variables
+if len(sys.argv) > 1:
+	RunMode = sys.argv[1]
+	
+if RunMode not in ['create', 'destroy']:
+	print "\n ERROR: \n - Add runmode argument after filename <create, destroy> (e.g.: python file.py create)"
+	exit()
+	
 # Nice UI function
 def PrintResult(response, newline=True):
 	httpCode = response['ResponseMetadata']['HTTPStatusCode']
@@ -46,11 +56,17 @@ os.system("cls")
 # Mission objective
 print 'S3-M4N!PUL4T0R3000 by DWR' + "\n"
 
+print 'Run mode:'
+print ' - {} \n'.format(RunMode.capitalize())
+
 print 'Objectives:'
-print ' - Create bucket "' + bucket_name + '"'
-print ' - Upload file "' + file_name + '"'
-print ' - Set file to public'
-print ' - Print out file link'
+if RunMode == 'create':
+	print ' - Create bucket "' + bucket_name + '"'
+	print ' - Upload file "' + file_name + '"'
+	print ' - Set file to public'
+	print ' - Print out file link'
+else:
+	print ' - Delete bucket "{}" and all it\'s children'.format(bucket_name)
 print ''
 
 
@@ -61,70 +77,71 @@ s3_resource = boto3.resource('s3')
 
 
 #####++++++++ ROLL UP ++++++++#######
-############# Create Bucket #######################
-# Create a bucket using a client
-print 'Creating bucket...'
-response = s3_client.create_bucket(Bucket=bucket_name)
-PrintResult(response)
+if RunMode == 'create':
+	############# Create Bucket #######################
+	# Create a bucket using a client
+	print 'Creating bucket...'
+	response = s3_client.create_bucket(Bucket=bucket_name)
+	PrintResult(response)
 
 
 
-############# List Buckets #######################
-# Print out bucket names
-print 'Buckets:'
-for bucket in s3_resource.buckets.all():
-    print(bucket.name)
-print ''
+	############# List Buckets #######################
+	# Print out bucket names
+	print 'Buckets:'
+	for bucket in s3_resource.buckets.all():
+		print ' - {}'.format(bucket.name)
+	print ''
 
-############ Upload file #################
-# Upload a new file in the new bucket
-print 'Uploading file....'
-data = open(file_folder_location+file_name, 'rb') # read-only (r), load bites (b)
-response = s3_resource.Bucket(bucket_name).put_object(Key=file_name, Body=data)
-if response:
-	print ' - {}'.format(response)
-	print ' - Done \n'
-else:
-	print ' - Failed'
+	############ Upload file #################
+	# Upload a new file in the new bucket
+	print 'Uploading file....'
+	data = open(file_folder_location+file_name, 'rb') # read-only (r), load bites (b)
+	response = s3_resource.Bucket(bucket_name).put_object(Key=file_name, Body=data)
+	if response:
+		print ' - {}'.format(response)
+		print ' - Done \n'
+	else:
+		print ' - Failed'
 
-############ List Objects + extra ########################
-# # List all objects in the new bucket
-print 'Items in ' + bucket_name + ":"
-for object in s3_resource.Bucket(bucket_name).objects.all():
-    print ' - {}'.format(object.key)
-print ''
+	############ List Objects + extra ########################
+	# # List all objects in the new bucket
+	print 'Items in ' + bucket_name + ":"
+	for object in s3_resource.Bucket(bucket_name).objects.all():
+		print ' - {}'.format(object.key)
+	print ''
 
-# # Get bucket acl
-# print 'ACL of ' + bucket_name + ':'
-# result = s3_client.get_bucket_acl(Bucket=bucket_name)
-# print(result)
-# print ''
+	# # Get bucket acl
+	# print 'ACL of ' + bucket_name + ':'
+	# result = s3_client.get_bucket_acl(Bucket=bucket_name)
+	# print(result)
+	# print ''
 
-# # Get file acl
-# print 'ACL of ' + file_name + ':'
-# result = s3_client.get_object_acl(Bucket=bucket_name, Key=file_name)
-# print(result)
-# print ''
+	# # Get file acl
+	# print 'ACL of ' + file_name + ':'
+	# result = s3_client.get_object_acl(Bucket=bucket_name, Key=file_name)
+	# print(result)
+	# print ''
 
-############ Edit file ###################
-# Set uploaded file to public
-print 'Setting ' + file_name + ' to public...'
-response = s3_client.put_object_acl(ACL='public-read', Bucket=bucket_name, Key=file_name)
-PrintResult(response)
+	############ Edit file ###################
+	# Set uploaded file to public
+	print 'Setting ' + file_name + ' to public...'
+	response = s3_client.put_object_acl(ACL='public-read', Bucket=bucket_name, Key=file_name)
+	PrintResult(response)
 
-########### List ########################
-# # Get file acl (check)
-# print 'ACL of ' + file_name + ':'
-# result = s3_client.get_object_acl(Bucket=bucket_name, Key=file_name)
-# print(result)
-# print ''
+	########### List ########################
+	# # Get file acl (check)
+	# print 'ACL of ' + file_name + ':'
+	# result = s3_client.get_object_acl(Bucket=bucket_name, Key=file_name)
+	# print(result)
+	# print ''
 
-# Print out file link
-print 'Download link ' + file_name + ':'
-print 'https://s3.amazonaws.com/' + bucket_name + '/' + file_name + '\n'
+	# Print out file link
+	print 'Download link ' + file_name + ':'
+	print 'https://s3.amazonaws.com/' + bucket_name + '/' + file_name + '\n'
 
 ####++++++ ROLL DOWN ++++#####
-if delBucket:
+elif RunMode == 'destroy':
 	print 'Deleting bucket...'
 	bucket = s3_resource.Bucket(bucket_name)
 	for key in bucket.objects.all():
